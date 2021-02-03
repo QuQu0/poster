4f21ecee3f488f7bb06fee80c59163a42703b513
Fierce <x-qu@163.com>
2021-02-03 09:27:18 +0800

    Update image_circle_local_handler.go

--- handler/image_circle_local_handler.go
+++ handler/image_circle_local_handler.go
@@ -10,29 +10,40 @@ package handler
 
 import (
 	"fmt"
+	"image"
+	"net/http"
+	"os"
+
 	"github.com/hitailang/poster/circlemask"
 	"github.com/hitailang/poster/core"
-	"image"
-	"os"
 )
+
 // ImageCircleLocalHandler 根据Path路径设置圆形图片
 type ImageCircleLocalHandler struct {
 	// 合成复用Next
 	Next
-	X   int
-	Y   int
+	X    int
+	Y    int
 	Path string //./images/xx.png
+	URL  string
 }
 
 // Do 地址逻辑
 func (h *ImageCircleLocalHandler) Do(c *Context) (err error) {
 
-	imageFile, err := os.Open(h.Path)
-	if err != nil {
-		fmt.Errorf("os.Open err：%v", err)
-	}
+	var srcImage image.Image
 
-	srcImage,_,err := image.Decode(imageFile)
+	if h.Path != "" {
+		imageFile, err := os.Open(h.Path)
+		if err != nil {
+			fmt.Errorf("os.Open err：%v", err)
+		}
+
+		srcImage, _, err = image.Decode(imageFile)
+	} else if h.URL != "" {
+		res, _ := http.Get(h.URL)
+		srcImage, _, err = image.Decode(res.Body)
+	}
 
 	if err != nil {
 		fmt.Errorf("SetRemoteImage image.Decode err：%v", err)
@@ -40,6 +51,9 @@ func (h *ImageCircleLocalHandler) Do(c *Context) (err error) {
 	// 算出图片的宽度和高试
 	width := srcImage.Bounds().Max.X - srcImage.Bounds().Min.X
 	hight := srcImage.Bounds().Max.Y - srcImage.Bounds().Min.Y
+
+	width = int(float64(width) * 0.8)
+	hight = int(float64(hight) * 0.8)
 
 	//把头像转成Png,否则会有白底
 	srcPng := core.NewPNG(0, 0, width, hight)
